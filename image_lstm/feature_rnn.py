86f700bab957a73b266ee732100c9821342a606f
AdrienMorvan <adrien.morvan@photobox.com>
2018-05-18 11:50:41 +0200

    Added changes for compatibility btw models

--- image_lstm/feature_rnn.py
+++ image_lstm/feature_rnn.py
@@ -7,17 +7,35 @@ class FeatureRNN(torch.nn.Module):
 
     def __init__(self, size_input, size_hidden, n_labels):
         super(FeatureRNN, self).__init__()
+
+        # Embbedding loader
+        self.embedding_loader = EmbeddingExtractor()
+
+        # RNN params
         self.size_hidden = size_hidden
         self.mixed_size = size_input + size_hidden
         self.i2h = torch.nn.Linear(self.mixed_size, size_hidden)
         self.i2o = torch.nn.Linear(self.mixed_size, n_labels)
         self.softmax = torch.nn.LogSoftmax(dim=1)
+        self.init_hidden()
 
-    def forward(self, feature, hidden):
-        mixed = torch.cat((feature, hidden), 1)
-        new_hidden = self.i2h(mixed)
+    def forward(self, photos):
+        if type(photos) != list:
+            photos = [photos]
+        features = self.build_batch(photos)
+
+        for feature in features:
+            mixed = torch.cat((feature, self.hidden), 1)
+            self.hidden = self.i2h(mixed)
+
         labels = self.softmax(self.i2o(mixed))
-        return labels, new_hidden
+        return labels
 
     def init_hidden(self):
-        return torch.zeros(1, self.size_hidden)
+        self.hidden = torch.zeros(1, self.size_hidden)
+
+    def build_batch(self, photos):
+        return [self.embedding_loader.forward(
+            self.embedding_loader.load_img_from_path( img_path)
+        ).view(1, -1)
+        for img_path in photos]
--- tests/test_feature_rnn.py
+++ tests/test_feature_rnn.py
@@ -6,28 +6,20 @@ from image_lstm.feature_rnn import FeatureRNN
 
 class TestFeatureRNN(TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.embedding_loader = EmbeddingExtractor()
         self.model = FeatureRNN(512, 512, 3)
 
     def test_forward(self):
-        feature = self.embedding_loader.forward(
-            self.embedding_loader.load_img_from_path(
-                "../CUFED_mini/images/0_7138083@N04/5271641595.jpg")
-        ).view(1, -1)
-        hidden = self.model.init_hidden()
-
-        labels, hidden = self.model.forward(feature, hidden)
+        self.model.init_hidden()
+        labels = self.model.forward(["../CUFED_mini/images/0_7138083@N04/5271641595.jpg",
+                                     "../CUFED_mini/images/0_7138083@N04/5271641595.jpg"])
         self.assertEqual(labels.size()[1], 3, "Bad number of labels")
 
     def test_backward(self):
-        feature = self.embedding_loader.forward(
-            self.embedding_loader.load_img_from_path(
-                "../CUFED_mini/images/0_7138083@N04/5271641595.jpg")
-        ).view(1, -1)
-        hidden = self.model.init_hidden()
-
-        labels, hidden = self.model.forward(feature, hidden)
+        self.model.init_hidden()
+        labels = self.model.forward("../CUFED_mini/images/0_7138083@N04/5271641595.jpg")
 
         loss_function = torch.nn.NLLLoss()
         loss = loss_function(labels, torch.LongTensor(1))
